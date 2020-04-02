2ae0e9f6a70c7343a1f120ef94c16d6bef07df31
JSybrandt <nalta45@gmail.com>
2020-04-02 15:08:16 -0400

    updates setup.py to fix pacakges

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parse_pubmed",
-    version="2020.04.02dev2",
+    version="2020.04.02dev3",
     author="Justin Sybrandt",
     author_email="justin@sybrandt.com",
     description="Parses the XML distribution of PubMed into pickles",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JSybrandt/ParsePubMed",
-    packages=[
+    packages=["parse_pubmed"],
+    install_requires=[
       "fire",
       "lxml",
       "tqdm",
